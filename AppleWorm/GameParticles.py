dbcc950bce99853210be4383bf32d19027234d77
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-01-22 23:24:36 -0500

    background effects

--- AppleWorm/GameParticles.py
+++ AppleWorm/GameParticles.py
@@ -1,4 +1,3 @@
-
 import pygame
 import numpy.random as random
 
--- AppleWorm/__init__.py
+++ AppleWorm/__init__.py
@@ -104,6 +104,81 @@ def create_player_death_explosion(
     )
 
 
+display = pygame.Surface((300, 200))
+display_bg = (255, 255, 255)  # (20, 19, 39)
+
+
+def render_side_fog(GAME_WINDOW, game_time):
+
+    w, h = display.get_width(), display.get_height()
+
+    # spans the entire width, and 33% of the height
+    effect_1_percent = 0.33
+    effect1_width, effect1_height = w, h * effect_1_percent
+    effect1_color1 = (15, 10, 24)
+    effect1_color2 = (0, 0, 0)
+    var1 = effect1_height / 4
+
+    # spans the entire width, and 12.5% of the height
+    effect_2_percent = 0.125
+    effect_2_percent *= 2
+    effect2_width, effect2_height = w, h * effect_2_percent
+    effect2_color1 = (0, 0, 0)
+    effect2_color2 = (0, 2, 4)
+    var2 = effect2_height / 2
+
+    # clear old effects and set background
+    display.fill(display_bg)
+
+    # effect 1
+
+    b2_points = (
+        ((effect1_width, var1),)
+        + tuple(
+            (
+                effect1_width - (effect1_width / 30 * (i + 1) + math.sin((game_time + i * 120) / 10) * 8),
+                3 * (var1 + math.sin((game_time + i * 10) / 10) * 4),
+            )
+            for i in range(29)
+        )
+        + ((0, var1), (0, 0), (w, 0))
+    )
+    back_surf = pygame.Surface((effect1_width, effect1_height))
+    pygame.draw.polygon(back_surf, effect1_color1, b2_points)
+    back_surf.set_colorkey(effect1_color2)
+    display.blit(back_surf, (0, 0))
+    display.blit(pygame.transform.flip(back_surf, False, True), (0, h - effect1_height))
+
+    # effect 2
+    b_points = (
+        ((0, var2),)
+        + tuple(
+            (w / 30 * (i + 1) + math.sin((game_time + i * 120) / 4) * 8, var2 + math.sin((game_time + i * 10) / 10) * 4)
+            for i in range(29)
+        )
+        + ((w, var2), (w, 0), (0, 0))
+    )
+
+    fog_surf = pygame.Surface((effect2_width, effect2_height))
+    pygame.draw.polygon(fog_surf, effect2_color2, b_points)
+    fog_surf.set_alpha(150)
+    fog_surf.set_colorkey(effect2_color1)
+
+    display.blit(pygame.transform.flip(fog_surf, True, False), (0, -6))
+    display.blit(fog_surf, (0, 0))
+    display.blit(pygame.transform.flip(fog_surf, True, True), (0, h - effect2_height + 6))
+    display.blit(pygame.transform.flip(fog_surf, False, True), (0, h - effect2_height))
+
+    side_fog = pygame.transform.scale(pygame.transform.rotate(fog_surf, 90), (effect2_height, h))
+    display.blit(pygame.transform.flip(side_fog, False, True), (-6, 0))
+    display.blit(side_fog, (0, 0))
+    display.blit(pygame.transform.flip(side_fog, True, True), (w - effect2_height, 0))
+    display.blit(pygame.transform.flip(side_fog, True, False), (w - effect2_height + 6, 0))
+
+    # actually render the effect to the game window
+    GAME_WINDOW.blit(pygame.transform.scale(display, GAME_WINDOW.get_size()), (0, 0))
+
+
 def main():
     set_dpi_aware()
 
@@ -111,7 +186,7 @@ def main():
     pygame.init()
 
     # set up window, important this comes before Sprites.init()
-    GAME_WINDOW = pygame.display.set_mode((GC.WIDTH, GC.HEIGHT))
+    GAME_WINDOW = pygame.display.set_mode((GC.WIDTH, GC.HEIGHT), pygame.SCALED + pygame.RESIZABLE)
     GAME_WINDOW.fill((0, 0, 0))
 
     GA.Fonts.init()
@@ -139,12 +214,11 @@ def main():
 
     clock = pygame.time.Clock()
 
-    square_effects = []
-    square_effects2 = []
-
     square_effect = GameParticles.FallingSquareEffect(GA.Colors.BLACK)
     circle_effect = GameParticles.ExpandingCircle(GA.Colors.WHITE)
     player.bullet_effect_renders.append(circle_effect)
+
+    game_time = 0
 
     play_game = True
     while play_game:
@@ -153,6 +227,7 @@ def main():
         clock.tick(FRAME_RATE)
 
         game_ticks = pygame.time.get_ticks()
+        game_time += 1
 
         events = pygame.event.get()
         keys = pygame.key.get_pressed()
@@ -175,7 +250,9 @@ def main():
 
         GAME_WINDOW.blit(GA.Sprites.BACKGROUND1_SPRITE, GA.Sprites.BACKGROUND1_RECT)
 
-        square_effect.create_particle_on_chance(1 / 60)
+        render_side_fog(GAME_WINDOW, game_time)
+
+        square_effect.create_particle_on_chance(0.6)
         square_effect.render(GAME_WINDOW)
 
         GAME_WINDOW.blit(text, text_rect)
